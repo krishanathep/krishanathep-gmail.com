d280c89e9b09ef71df6a0420d6369b9510f2b4b9
krishanathep <krishanathep@gmail.com>
2020-03-30 16:54:48 +0700

    Updates

---
+++ precache-manifest.5c183a1b3c7a83d61e34e1d085387f13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b237935f843e3b2f3e44545b0ae809",
+    "url": "/react-firebase-app/index.html"
+  },
+  {
+    "revision": "eff2b8b36920b1658137",
+    "url": "/react-firebase-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c80f77036b6e038e5ef2",
+    "url": "/react-firebase-app/static/js/2.43229c94.chunk.js"
+  },
+  {
+    "revision": "c03a38235b49ce3de8834e76429c16a7",
+    "url": "/react-firebase-app/static/js/2.43229c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff2b8b36920b1658137",
+    "url": "/react-firebase-app/static/js/main.2c9cc884.chunk.js"
+  },
+  {
+    "revision": "7a870ac045b0e9c33809",
+    "url": "/react-firebase-app/static/js/runtime-main.8f0e13ed.js"
+  }
+]);
