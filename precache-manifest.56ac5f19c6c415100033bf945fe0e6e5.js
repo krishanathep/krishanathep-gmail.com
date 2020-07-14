54fe761e09ae66a8f89ea738332c1c550791f723
krishanathep <krishanathep@gmail.com>
2020-07-14 16:24:39 +0700

    Updates

---
+++ precache-manifest.56ac5f19c6c415100033bf945fe0e6e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1935952e7e96e3762d96a77c1db5f665",
+    "url": "/react-firebase-app/index.html"
+  },
+  {
+    "revision": "99deae0151544f816544",
+    "url": "/react-firebase-app/static/css/main.6f3279f0.chunk.css"
+  },
+  {
+    "revision": "3971303669ecb34ac69c",
+    "url": "/react-firebase-app/static/js/2.d57cfb1b.chunk.js"
+  },
+  {
+    "revision": "eb7c3014fa7bcc7c67b79ff0baa3150a",
+    "url": "/react-firebase-app/static/js/2.d57cfb1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99deae0151544f816544",
+    "url": "/react-firebase-app/static/js/main.c47e7cc4.chunk.js"
+  },
+  {
+    "revision": "7a870ac045b0e9c33809",
+    "url": "/react-firebase-app/static/js/runtime-main.8f0e13ed.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0b5ad7bd428db005b33d83b5ed17888b.js"
+  "/react-firebase-app/precache-manifest.56ac5f19c6c415100033bf945fe0e6e5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-firebase-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
